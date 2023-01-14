86ff85ff3a3e51daa6c2ba2fad9fd5fd701bc519
Srikrushna221 <61017540+Srikrushna221@users.noreply.github.com>
2023-01-14 14:24:47 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+'use strict'
+
+module.exports = {
+    mailer: {
+        service: 'Gmail',
+        auth: {
+            user: 'tempmail@gmail.com',
+            pass: 'password'
+        }
+    },
+    dbConnstring: 'mongodb://127.0.0.1:27017/codeshare',
+    sessionKey: 'session123'
+
+}
